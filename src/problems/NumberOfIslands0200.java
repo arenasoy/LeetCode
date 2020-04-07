028c63c65e5703c150dca92fbdd60aede9666549
Carolina <caro.arenas2607@gmail.com>
2020-04-07 13:40:23 -0300

    Number of Islands #0200 - pass matrix as parameter

--- src/problems/NumberOfIslands0200.java
+++ src/problems/NumberOfIslands0200.java
@@ -2,31 +2,27 @@ package problems;
 
 public class NumberOfIslands0200 {
 
-    private char m[][];
-    
     public int numIslands(char[][] grid) {
-        
-        this.m = grid;
-        
+            
         int count = 0;
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                count += verify(i, j);        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                count += verify(grid, i, j);        
             }
         }
         return count;
     }
     
-    private int verify(int i, int j) {
-        if (i >= m.length || i < 0 || j >= m[0].length || j < 0)
+    private int verify(char grid[][], int i, int j) {
+        if (i >= grid.length || i < 0 || j >= grid[0].length || j < 0)
             return 0;
         
-        if (m[i][j] == '1') {
-            m[i][j] = 'v';
-            verify(i, j+1);
-            verify(i, j-1);
-            verify(i+1, j);
-            verify(i-1, j);
+        if (grid[i][j] == '1') {
+            grid[i][j] = 'v';
+            verify(grid, i, j+1);
+            verify(grid, i, j-1);
+            verify(grid, i+1, j);
+            verify(grid, i-1, j);
             return 1;
         }
         
