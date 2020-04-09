3b8406454e4475369dc7e3c459feaef645182206
Carolina <caro.arenas2607@gmail.com>
2020-04-08 23:46:53 -0300

    Midle of the Linked List - no number

---
+++ src/problems/MidleOfTheLinkedList.java
@@ -0,0 +1,30 @@
+package problems;
+
+public class MidleOfTheLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        
+        if (head == null)
+            return null;
+        
+        ListNode one = head;
+        ListNode two = head;
+        
+        while (two.next != null) {
+            one = one.next;
+            two = two.next;
+            
+            if (two.next != null)
+                two = two.next;
+        }
+        
+        return one;
+    }
+
+}
