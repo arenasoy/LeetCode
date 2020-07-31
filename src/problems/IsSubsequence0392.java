226de925d2ab089e0301e559f4c2aa268330d029
Carolina <caro.arenas2607@gmail.com>
2020-07-31 10:15:46 -0300

    Is Subsequence #0392

---
+++ src/problems/IsSubsequence0392.java
@@ -0,0 +1,22 @@
+package problems;
+
+public class IsSubsequence0392 {
+    public boolean isSubsequence(String s, String t) {
+        
+        int i = 0;
+        int j = 0;
+        
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        
+        if (i == s.length()) {
+            return true;
+        }
+        
+        return false;
+    }
+}
